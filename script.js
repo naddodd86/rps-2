507d36e320a8d910a845f5cd2a6ff3acb049ca98
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-21 16:15:24 -0600

    add score and round counters

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // getComputerChoice function that will randomly return either 'Rock, Paper or Scissors'
 
+
 function getComputerChoice()    {
-    const randomChoice = Math.floor(Math.random() * 3 );
+    let randomChoice = Math.floor(Math.random() * 3 );
     
    
     switch(randomChoice)    {
@@ -17,34 +18,96 @@ function getComputerChoice()    {
     }   
 
 }
-// Write a function that plays a single round of RPS. (Should use two parameters: playerSelection and computerSelection) and 
-
-// Return the results of this function.
+        
 
 function playRound(playerSelection, computerSelection) {
-     
+        
+         
+
     if    (playerSelection == computerSelection)
             roundResult = 'It\'s a Draw!';
+            
     
      else if (playerSelection == 'rock' && computerSelection == 'paper') {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
+            computerScore++;
+
+            
+            
     } else if (playerSelection == 'rock' && computerSelection == 'scissors')  {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
+            playerScore++;
+            
 
     } else if (playerSelection == 'scissors' && computerSelection == 'rock')  {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
+             computerScore++;
+
     } else if (playerSelection == 'scissors' && computerSelection == 'paper')    {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
+            playerScore++;
+            
 
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
+            playerScore++;
+            
+
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
+            computerScore++;
+            
     }
     return roundResult;
+       
+   
+    
 }
- 
 
-const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
-const computerSelection = getComputerChoice();
+
+ // New function called game(). Use previous function inside of this one to play 5 round game that 
+ // keeps score and reports a winner or loser at the end.
+
+ function game()        {
+        let roundCount = parseInt(0);
+        playRound(playerSelection, computerSelection);
+        roundCount++; 
+        console.log(roundCount);
+        console.log(roundResult);
+          
+        playRound(playerSelection, computerSelection);
+        roundCount++;
+        console.log(roundCount); 
+        console.log(roundResult); 
+        playRound(playerSelection, computerSelection);
+        roundCount++; 
+        console.log(roundCount);
+        console.log(roundResult); 
+        playRound(playerSelection, computerSelection);
+        roundCount++; 
+        console.log(roundCount);
+        console.log(roundResult); 
+        playRound(playerSelection, computerSelection);
+        roundCount++;  
+        console.log(roundCount);
+        console.log(roundResult);
+        
+
+        
+           
+
+ }
+
+        const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
+        const computerSelection = getComputerChoice();  
+        let playerScore = parseInt(0);
+        let computerScore = parseInt(0);
+
+
+
 console.log(playRound(playerSelection, computerSelection));
+console.log(playerScore);
+console.log(computerScore);
+//console.log(round);
+console.log(game());
+
