10c117a79be058922c3a31b19e7e7d83ddf7bcdb
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-21 12:07:20 -0600

    Finish getComputerChoice

--- script.js
+++ script.js
@@ -3,21 +3,32 @@
 function getComputerChoice()    {
     const randomChoice = Math.floor(Math.random() * 3 );
     
-    switch (randomChoice) {
-        case 0 :  
+   
+    switch(randomChoice)    {
+        case 0 : 
             return 'rock';
             break;
         case 1 : 
             return 'paper';
             break;
+        case 2 :
             return 'scissors';
-            break;
-        default:
-            console.log('Invalid');
-            break;
-
-    }
+        default : 
+            return 'Something went so so wrong!';
+    }   
 
 }
-console.log(getComputerChoice);
+console.log(getComputerChoice());
 
+// Write a function that plays a single round of RPS. (Should use two parameters: playerSelection and computerSelection) and 
+// return a string that declares the winner. ( make the function's playerSelection case insensitive)
+// Return the results of this function.
+
+function playRound( playerSelection, computerSelection) {
+
+
+}
+
+const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", ' ').toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
