180b92308ce3112347d2163adc8e3397247292c7
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-21 13:13:58 -0600

    fix template literals

--- script.js
+++ script.js
@@ -13,43 +13,38 @@ function getComputerChoice()    {
             break;
         case 2 :
             return 'scissors';
-        default : 
-            return 'Something went so so wrong!';
+        
     }   
 
 }
-//console.log(getComputerChoice());
-
 // Write a function that plays a single round of RPS. (Should use two parameters: playerSelection and computerSelection) and 
-// return a string that declares the winner. ( make the function's playerSelection case insensitive)
+
 // Return the results of this function.
 
 function playRound(playerSelection, computerSelection) {
      
-    if    (playerSelection === computerSelection)
+    if    (playerSelection == computerSelection)
             roundResult = 'It\'s a Draw!';
     
-     else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            roundResult = ' You lose. `${computerSelection} beats ${playerSelection}!`';
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors')  {
-            roundResult = ' You win. `${playerSelection} beats ${computerSelection}`!';
+     else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors')  {
+            roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
 
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock')  {
-            roundResult = ' You lose. `${computerSelection} beats ${playerSelection}!`';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper')    {
-            roundResult = ' You win. `${playerSelection} beats ${computerSelection}`!';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock')  {
+            roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper')    {
+            roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
 
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            roundResult = ' You win. `${playerSelection} beats ${computerSelection}`!';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            roundResult = ' You lose. `${computerSelection} beats ${playerSelection}!`';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
     }
     return roundResult;
 }
-    
+ 
 
-
-
-const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", ' ').toLowerCase();
+const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
