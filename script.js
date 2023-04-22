47715cf7f4f648e58753251c028a29257f6e517a
naddodd86 <126707721+naddodd86@users.noreply.github.com>
2023-04-21 18:45:28 -0600

    stuck on score fix game()

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // getComputerChoice function that will randomly return either 'Rock, Paper or Scissors'
-
+let roundCount = 0;
+ let playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
+ let computerSelection = getComputerChoice();
+ 
 
 function getComputerChoice()    {
     let randomChoice = Math.floor(Math.random() * 3 );
@@ -14,102 +17,106 @@ function getComputerChoice()    {
             break;
         case 2 :
             return 'scissors';
-        
-    }   
+        }   
 
 }
         
 
 function playRound(playerSelection, computerSelection) {
-         
+        let playerScore = 0;
+        let computerScore = 0;
+
         playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
         computerSelection = getComputerChoice();
-        ssss
-    if    (playerSelection == computerSelection)
+        
+    if    (playerSelection == computerSelection) {
+
             roundResult = 'It\'s a Draw!';
+            playerScore += 0;
+            computerScore += 0;
             
-    
-     else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            
+
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
-            computerScore++;
+            computerScore += 1; 
 
             
             
     } else if (playerSelection == 'rock' && computerSelection == 'scissors')  {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
-            playerScore++;
+            playerScore += 1;
             
 
     } else if (playerSelection == 'scissors' && computerSelection == 'rock')  {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
-             computerScore++;
+             computerScore += 1;
+
 
     } else if (playerSelection == 'scissors' && computerSelection == 'paper')    {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
-            playerScore++;
+            playerScore += 1;
             
 
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
             roundResult = `' You win. ${playerSelection} beats ${computerSelection}!'`;
-            playerScore++;
+            playerScore += 1;
             
 
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
             roundResult = `' You lose. ${computerSelection} beats ${playerSelection}!'`;
-            computerScore++;
+            computerScore += 1;
             
     }
-    return roundResult;
+   console.log(playerScore);
+   console.log(computerScore); 
+   return roundResult;
     
        
-   
-    
 }
-
 
  // New function called game(). Use previous function inside of this one to play 5 round game that 
  // keeps score and reports a winner or loser at the end.
 
  function game()        {
-        let roundCount = parseInt(0);
-        playRound(playerSelection, computerSelection);
+        
+        roundCount = 0;
+
+ playRound(playerSelection, computerSelection);
         roundCount++; 
         console.log(roundCount);
         console.log(roundResult);
-          
-        playRound(playerSelection, computerSelection);
+
+playRound(playerSelection, computerSelection);
         roundCount++;
         console.log(roundCount); 
         console.log(roundResult); 
-        playRound(playerSelection, computerSelection);
+playRound(playerSelection, computerSelection);
         roundCount++; 
         console.log(roundCount);
         console.log(roundResult); 
-        playRound(playerSelection, computerSelection);
+playRound(playerSelection, computerSelection);
         roundCount++; 
         console.log(roundCount);
         console.log(roundResult); 
-        playRound(playerSelection, computerSelection);
+playRound(playerSelection, computerSelection);
         roundCount++;  
         console.log(roundCount);
         console.log(roundResult);
+
+return;
+}
+
+  
         
-
-        
-           
-
- }
-
- const playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors!", '').toLowerCase();
- const computerSelection = getComputerChoice();   
-        let playerScore = parseInt(0);
-        let computerScore = parseInt(0);
 
 game();
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerScore);
-console.log(computerScore);
+
+//console.log(playRound(playerSelection, computerSelection));
+//console.log(playerScore);
+//console.log(computerScore);
 //console.log(round);
 console.log(game());
+
 
